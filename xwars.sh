82e90e3884fe66297c7aaf4c542085296e0a81f7
root <root@LAPTOP-NQVJ46VT.localdomain>
2022-01-28 19:30:37 -0500

    Added variable input check

--- xwars.sh
+++ xwars.sh
@@ -3,24 +3,35 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+if [ -z "${XIP}" ]
+then 
+	echo "Unable to find \$XIP"
+	echo "...exiting..."
+	sleep 1
+	exit
+else
+	echo "Found ESP32 at $XIP"
+	IP=$XIP
+fi
+
 while :
 do
 	echo -e "${RED}Red Laser - AT-AT ${NC}- 7 seconds"
-	curl -o /dev/null -s "192.168.1.71/win&PL=4"
+	curl -o /dev/null -s "$IP/win&PL=4"
 	sleep 7 
 	echo -e "${GREEN}Green Laser - Santa ${NC} - 4 seconds"
-	curl -o /dev/null -s "192.168.1.71/win&PL=1"
+	curl -o /dev/null -s "$IP/win&PL=1"
 	sleep 4
 	echo -e "${RED}Red Laser - AT-AT ${NC}- 5 seconds"
-	curl -o /dev/null -s "192.168.1.71/win&PL=4"
+	curl -o /dev/null -s "$IP/win&PL=4"
 	sleep 5
 	echo -e "${GREEN}Green Laser - Santa ${NC}- 3 seconds"
-	curl -o /dev/null -s "192.168.1.71/win&PL=1"
+	curl -o /dev/null -s "$IP/win&PL=1"
 	sleep 3
 	echo "Fireburst for 8 seconds"
-	curl -o /dev/null -s "192.168.1.71/win&PL=6"
+	curl -o /dev/null -s "$IP/win&PL=6"
 	sleep 8
 	echo "Lights Off for 3 seconds"
-	curl -o /dev/null -s "192.168.1.71/win&PL=5"
+	curl -o /dev/null -s "$IP/win&PL=5"
 	sleep 3
 done
